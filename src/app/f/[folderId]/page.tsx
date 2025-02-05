ece1aabb9708539fb9bc1d4d598006e754a957f0
João Paulo <jpbsampaio@hotmail.com>
2025-02-05 15:10:18 -0300

    fix breadcrumbs

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,31 +1,52 @@
 import { db } from "~/server/db";
 import {
-  files as filesSchema,
-  folders as foldersSchema,
+    files as filesSchema,
+    folders as foldersSchema,
 } from "~/server/db/schema";
 import DriveContents from "../../drive-contents";
-import { z } from "zod";
 import { eq } from "drizzle-orm";
 
+async function getAllParents(folderId: number) {
+    const parents = [];
+    let currentId: number | null = folderId;
+    while (currentId !== null) {
+        const folder = await db
+            .selectDistinct()
+            .from(foldersSchema)
+            .where(eq(foldersSchema.id, currentId));
+
+        if (!folder[0]) {
+            throw new Error("Folder not found");
+        }
+        parents.unshift(folder[0]);
+        currentId = folder[0]?.parent;
+    }
+    return parents;
+}
+
 export default async function GoogleDriveClone(props: {
-  params: Promise<{ folderId: string }>;
+    params: Promise<{ folderId: string }>;
 }) {
-  const params = await props.params;
+    const params = await props.params;
 
-  const parsedFolderId = parseInt(params.folderId);
-  if (isNaN(parsedFolderId)) {
-    return <div>Invalid folder ID</div>;
-  }
+    const parsedFolderId = parseInt(params.folderId);
+    if (isNaN(parsedFolderId)) {
+        return <div>Invalid folder ID</div>;
+    }
 
-  const folders = await db
-    .select()
-    .from(foldersSchema)
-    .where(eq(foldersSchema.parent, parsedFolderId));
+    const foldersPromise = db
+        .select()
+        .from(foldersSchema)
+        .where(eq(foldersSchema.parent, parsedFolderId));
 
-  const files = await db
-    .select()
-    .from(filesSchema)
-    .where(eq(filesSchema.parent, parsedFolderId));
+    const filesPromise = db
+        .select()
+        .from(filesSchema)
+        .where(eq(filesSchema.parent, parsedFolderId));
 
-  return <DriveContents files={files} folders={folders} />;
+    const parentsPromise = getAllParents(parsedFolderId);
+
+    const [files, folders, parents] = await Promise.all([filesPromise, foldersPromise, parentsPromise]);
+
+    return <DriveContents files={files} folders={folders} parents={parents} />;
 }
