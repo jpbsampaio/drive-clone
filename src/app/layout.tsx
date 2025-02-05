f83a8a4b240d061b93909e35325802b45dbbb5c6
João Paulo <jpbsampaio@hotmail.com>
2025-02-05 17:37:44 -0300

    Adds Auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import type React from "react"
-
+import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
@@ -16,9 +16,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className="dark">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
 
