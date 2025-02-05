ae7df5f4c91f98924914940fbf6209410e2a143b
João Paulo <jpbsampaio@hotmail.com>
2025-02-05 10:39:39 -0300

    adds DB

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,19 @@
 import { type Config } from "drizzle-kit";
-
+import * as fs from "fs";
 import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
+  dialect: "singlestore",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    host: env.SINGLESTORE_HOST,
+    port: parseInt(env.SINGLESTORE_PORT),
+    user: env.SINGLESTORE_USER,
+    password: env.SINGLESTORE_PASS,
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {
+      ca: fs.readFileSync("./certs/singlestore_bundle.pem").toString()
+    },
   },
   tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
