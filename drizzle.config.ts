d41344e1c9dbdf555ffbffd68cccb2547ee08b2b
João Paulo <jpbsampaio@hotmail.com>
2025-02-05 13:34:15 -0300

    resolve SSL conection

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,17 @@
 import { type Config } from "drizzle-kit";
-import * as fs from "fs";
+
 import { env } from "~/env";
 
 export default {
   schema: "./src/server/db/schema.ts",
   dialect: "singlestore",
+  tablesFilter: ["drive_tutorial_*"],
   dbCredentials: {
     host: env.SINGLESTORE_HOST,
     port: parseInt(env.SINGLESTORE_PORT),
     user: env.SINGLESTORE_USER,
     password: env.SINGLESTORE_PASS,
-    database: env.SINGLESTORE_DB_NAME
+    database: env.SINGLESTORE_DB_NAME,
+    ssl: {},
   },
-  tablesFilter: ["drive-tutorial_*"],
 } satisfies Config;
