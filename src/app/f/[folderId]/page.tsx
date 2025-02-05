5bc1e845a133206a6c8ac7ee640139ba1d3772ad
João Paulo <jpbsampaio@hotmail.com>
2025-02-05 15:28:32 -0300

    refactor `queries.ts`

--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,23 +1,21 @@
 import DriveContents from "../../drive-contents";
-import { getAllParentsForFolder, getFiles, getFolders } from "~/server/db/queries";
+import { QUERIES } from "~/server/db/queries";
 
 export default async function GoogleDriveClone(props: {
-    params: Promise<{ folderId: string }>;
+  params: Promise<{ folderId: string }>;
 }) {
-    const params = await props.params;
+  const params = await props.params;
 
-    const parsedFolderId = parseInt(params.folderId);
-    if (isNaN(parsedFolderId)) {
-        return <div>Invalid folder ID</div>;
-    }
+  const parsedFolderId = parseInt(params.folderId);
+  if (isNaN(parsedFolderId)) {
+    return <div>Invalid folder ID</div>;
+  }
 
-    const foldersPromise = getFolders(parsedFolderId);
+  const [folders, files, parents] = await Promise.all([
+    QUERIES.getFolders(parsedFolderId),
+    QUERIES.getFiles(parsedFolderId),
+    QUERIES.getAllParentsForFolder(parsedFolderId),
+  ]);
 
-    const filesPromise = getFiles(parsedFolderId);
-
-    const parentsPromise = getAllParentsForFolder(parsedFolderId);
-
-    const [files, folders, parents] = await Promise.all([filesPromise, foldersPromise, parentsPromise]);
-
-    return <DriveContents files={files} folders={folders} parents={parents} />;
+  return <DriveContents files={files} folders={folders} parents={parents} />;
 }
